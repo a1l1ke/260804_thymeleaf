a5081798e38934a5da6d4eb2f4ced42f2b13e4c2
a1l1ke <onetenthplease@gmail.com>
2026-08-04 11:25:29 +0900

    feat: add msg2 using @Value annotation and render using thymeleaf inline syntax

--- src/main/java/org/example/thymeleaf/controller/MainController.java
+++ src/main/java/org/example/thymeleaf/controller/MainController.java
@@ -2,6 +2,7 @@ package org.example.thymeleaf.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.thymeleaf.config.AppProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MainController {
     private final AppProperties appProperties;
+//    private final String msg;
+//
+//    public MainController(
+//            AppProperties appProperties,
+//            @Value("${app.message}") String msg) {
+//        this.appProperties = appProperties;
+//        this.msg = msg;
+//    }
+
+    @Value("${app.message}")
+    private String msg; // 일종의 필드 주입
 
     @GetMapping
     public String index(Model model) {
         model.addAttribute("msg", appProperties.message());
+        model.addAttribute("msg2", msg);
         return "index";
     }
 }
